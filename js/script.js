fd35f52cfa9cb83703c1293c20a0a06e85237bf1
lukaszplac123 <lukasz.plac@gmail.com>
2017-09-01 19:21:50 +0200

    Next release. Trello-like app created.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $(function() {
+	var board = {
+    	name: 'Kanban Board',
+    	addColumn: function(column) {
+      		this.$element.append(column.$element);
+      		initSortable();
+    		},
+    	$element: $('#board .column-container')
+	};
+
+
 	function randomString() {
     var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
     var str = '';
@@ -7,6 +17,19 @@ $(function() {
     }
     return str;
 	}
+
+	function initSortable() {
+	   	$('.column-card-list').sortable({
+	    	connectWith: '.column-card-list',
+	    	placeholder: 'card-placeholder'
+	   	}).disableSelection();
+ 	}
+
+ 	$('.create-column').click(function(){
+		var name = prompt('Enter a column name');
+		var column = new Column(name);
+	    board.addColumn(column);
+  	});
 
 	function Column(name) {
 		var self = this;
@@ -46,4 +69,45 @@ $(function() {
 	    }
 	};
 
+	function Card(description) {
+		var self = this;
+		this.id = randomString();
+		this.description = description;
+		this.$element = createCard();
+
+		function createCard() {
+			var $card = $('<li>').addClass('card');
+			var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+			var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+			$cardDelete.click(function(){
+        	self.removeCard();
+			});
+			$card.append($cardDelete)
+				 .append($cardDescription);
+			return $card;
+		}
+	}
+	Card.prototype = {
+		removeCard: function() {
+			this.$element.remove();
+		}
+	}
+
+	// TWORZENIE KOLUMN
+	var todoColumn = new Column('To do');
+	var doingColumn = new Column('Doing');
+	var doneColumn = new Column('Done');
+
+	// DODAWANIE KOLUMN DO TABLICY
+	board.addColumn(todoColumn);
+	board.addColumn(doingColumn);
+	board.addColumn(doneColumn);
+
+	// TWORZENIE NOWYCH EGZEMPLARZY KART
+	var card1 = new Card('New task');
+	var card2 = new Card('Create kanban boards');
+
+	// DODAWANIE KART DO KOLUMN
+	todoColumn.addCard(card1);
+	doingColumn.addCard(card2);
 });
